1c4258ffd576d4a6313bab54927ad50a7e8953aa
Olukayode on WorkHQ <olukayode@ardentpros.com.ng>
2022-07-11 11:52:21 +0100

    First Commit
    This is the first commit of the custom app that we are building for pure water production, the major doctyoes have been added and we have started testing with dummy data

---
+++ config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "Pure Water Prod App",
+			"color": "grey",
+			"icon": "octicon octicon-file-directory",
+			"type": "module",
+			"label": _("Pure Water Prod App")
+		}
+	]
---
+++ pure_water_prod_app/doctype/operator/test_operator.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, ArdentPros (OptimaERP) and Contributors
+# See license.txt
+
+# import frappe
+import unittest
+
+class TestOperator(unittest.TestCase):
+	pass
