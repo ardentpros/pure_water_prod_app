43dd3cd3445b0de6b6554398cf8a63a5258b0552
Olukayode on WorkHQ <olukayode@ardentpros.com.ng>
2022-07-11 12:04:16 +0100

    Second Commit
    Commmitting the root folder of the custom app

---
+++ pure_water_prod_app/pure_water_prod_app/doctype/operator/operator.js
@@ -0,0 +1,8 @@
+// Copyright (c) 2022, ArdentPros (OptimaERP) and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Operator', {
+	// refresh: function(frm) {
+
+	// }
+});
---
+++ pure_water_prod_app/pure_water_prod_app/doctype/operator/operator.py
@@ -0,0 +1,8 @@
+# Copyright (c) 2022, ArdentPros (OptimaERP) and contributors
+# For license information, please see license.txt
+
+# import frappe
+from frappe.model.document import Document
+
+class Operator(Document):
+	pass
